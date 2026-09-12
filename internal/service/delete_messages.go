41ca6c655dc745bd69f50d14e6e37b4aeb5c84cb
Evgenii Medvedev <sibnetpwnz@yandex.ru>
2026-09-12 13:22:03 +0700

    rm print, nit rename

--- internal/service/delete_messages.go
+++ internal/service/delete_messages.go
@@ -1,14 +1,11 @@
 package service
 
-import "fmt"
-
 func (s *MessagesService) DeleteMessage(id uint64) bool {
-	fmt.Println("delete id ", id)
 	s.Mutex.Lock()
 	defer s.Mutex.Unlock()
-	_, hasId := s.Storage[id]
-	if hasId {
+	_, hasID := s.Storage[id]
+	if hasID {
 		delete(s.Storage, id)
 	}
-	return hasId
+	return hasID
 }
