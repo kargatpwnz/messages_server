75e22cc8e1a92fce4516a35b328e2263cc0996b3
Evgenii Medvedev <sibnetpwnz@yandex.ru>
2026-09-12 13:21:40 +0700

    json omitzero

--- internal/model/message.go
+++ internal/model/message.go
@@ -5,5 +5,5 @@ import "time"
 type Message struct {
 	ID        uint64    `json:"id"`
 	Message   string    `json:"message"`
-	CreatedAt time.Time `json:"created_at"`
+	CreatedAt time.Time `json:"created_at,omitzero"`
 }
